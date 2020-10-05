dabf5ab4b5e5bb6675bf9ebbb215f77f6f09abed
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-05 10:44:23 -0400

    Update Main.java
    test

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * prints out a numbers factors
+ * @Chad 
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates Scanner for user input
+    Scanner input = new Scanner(System.in);
     
-  }
+    // asks user for a number and records it for later
+    System.out.println("Enter a positive integer to determine its factors:");
+    int number = input.nextInt();
+    //variables here
+    int count = 0;
+    int quotientCheck = 10;
+    //Prints the starting message to the screen
+    System.out.println("The factors of " + number + " are:");
+    //does the loop while the numbers that have to be checked go up until they reach the user inputted number
+    while (count < number){ 
+      count = count + 1;
+      quotientCheck = number % count;
+     if (quotientCheck == 0){
+       System.out.println(count);
+      } 
+    } 
+  } 
 }
